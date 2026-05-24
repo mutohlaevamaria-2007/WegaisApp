c679bba573c5f7b20621b4dff5c684cd363541d1
Maria <mutohlaevamaria@gmail.com>
2026-05-24 16:00:05 +0500

    Отображение иконки текущей погоды

--- WegaisApp.Core/WeatherAPIIntegration/WeatherClient.cs
+++ WegaisApp.Core/WeatherAPIIntegration/WeatherClient.cs
@@ -9,12 +9,20 @@ namespace WegaisApp.Core.WeatherAPIIntegration
 {
     public class WeatherClient
     {
-        private static string url = "https://api.open-meteo.com/v1/forecast?latitude=54.7431&longitude=55.9678&current=temperature_2m,weather_code&timezone=auto";
-
-        public async Task<WeatherResponse> GetCurrentWeatherAsync()
+        private static Dictionary<string, Coords> _locations = new()
         {
+            { "Ufa" , new Coords(54.7431f, 55.9678f)},
+            { "hellish-cold", new Coords(-90f, -90f)} // FIX: тестовый случай
+        };
+
+        public async Task<WeatherResponse> GetCurrentWeatherAsync(string locationName = "Ufa")
+        {
+            Coords location = _locations[locationName];
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={location.latitude}&longitude={location.longitude}&current=temperature_2m,weather_code&timezone=auto";
             using HttpClient client = new HttpClient();
             return await client.GetFromJsonAsync<WeatherResponse>(url);
         }
+
+        public record struct Coords(float latitude, float longitude);
     }
 }
---
+++ WegaisApp.WPF/WeatherCodeToIconPathConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WegaisApp.WPF
+{
+    public class WeatherCodeToIconPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = value switch
+            {
+                0 => "clear",
+                1 or 2 => "partly-cloudy",
+                3 => "cloudy",
+                45 or 48 => "fog",
+                51 or 53 or 55 or 61 or 63 or 65 => "rain",
+                56 or 57 or 66 or 67 or 85 or 86 => "freezing-rain",
+                71 or 73 or 75 or 77 => "snow",
+                80 or 81 or 82 => "heavy-rain",
+                >= 95 => "thunderstorm",
+                _ => "unknow"
+            };
+
+            return $"pack://application:,,,/WegaisApp.WPF;component/WeatherIcons/{name}.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
